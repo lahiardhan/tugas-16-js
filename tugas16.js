f1a4ad52da7b44bbbd5a7ecf23333651991dfdd3
lahiardhan <lahia.ardhanm@gmail.com>
2021-08-25 16:58:51 +0700

    Tugas 16 JS
    Tugas ini saya serahkan kepada Tim Niomic

---
+++ tugas16.js
@@ -0,0 +1,21 @@
+function firstData() {
+   let min_x = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+   let max_x = Math.max(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+
+   console.log('FIRST DATA')
+   console.log('Nilai Minimal : ' + min_x);
+   console.log('Nilai Maximal : ' + max_x);
+}
+firstData()
+
+console.log("-------------------------")
+
+function secondData() {
+   let min_x = Math.min(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+   let max_x = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+
+   console.log('SECOND DATA')
+   console.log('Nilai Minimal : ' + min_x);
+   console.log('Nilai Maximal : ' + max_x);
+}
+secondData()
